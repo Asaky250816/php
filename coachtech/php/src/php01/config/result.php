5ca03b6fa3e52f79e5b878ee7cbb505bf10c081d
asaky <asaukiuki@gmail.com>
2026-01-14 20:30:54 +0900

    不要なファイルを削除

--- coachtech/php/src/php01/config/result.php
+++
@@ -1,8 +0,0 @@
-<?php
-$name = htmlspecialchars($_POST["name"], ENT_QUOTES);
-$product = htmlspecialchars($_POST["product"], ENT_QUOTES);
-$quantity = htmlspecialchars($_POST["quantity"], ENT_QUOTES);
-
-print "お名前は" . $name . "ですね" . "<br />";
-print "ご注文の商品は、" . $product . "<br />";
-print "注文数は" . $quantity;
